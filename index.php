b4fb8fed95666b0da10bd115ab5e1eca3773d330
Adrian <adrianvazquezsanchezart@gmail.com>
2025-01-10 13:51:50 +0100

    cambios index apis

--- index.php
+++ index.php
@@ -40,12 +40,12 @@ $posts = array_slice($posts, 0, 10);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabla de Tareas</title>
+    <title>Tabla de Tareas de APIs</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
-    <h1 class="mt-5 text-center">Tareas</h1>
+    <h1 class="mt-5 text-center">Tabla de APIs</h1>
     <table class="table table-striped mt-3">
         <thead>
             <tr>

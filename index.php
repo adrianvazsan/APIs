2cb9334b6a52d4dbdd35eab6d775843806b79931
Adrian <adrianvazquezsanchezart@gmail.com>
2025-01-10 14:03:13 +0100

    Ejercicio cambio para que sea APIs de lista de usuarios

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-$url = 'https://jsonplaceholder.typicode.com/todos';
+$url = 'https://jsonplaceholder.typicode.com/users';
 
 // Inicializa una sesión cURL
 $ch = curl_init($url);
@@ -18,54 +17,51 @@ if ($response === false) {
     echo 'Error en la solicitud: ' . curl_error($ch);
     exit();
 }
-
 // Cierra la sesión cURL
 curl_close($ch);
 
 // Decodifica la respuesta JSON.
-$posts = json_decode($response, true);
-
+$users = json_decode($response, true);
 // Verifica si la decodificación fue exitosa.
+
 if (json_last_error() !== JSON_ERROR_NONE) {
     echo 'Error al decodificar JSON: ' . json_last_error_msg();
     exit();
 }
-
-// Limita el número de tareas a mostrar a 10.
-$posts = array_slice($posts, 0, 10);
+// Limita el número de usuarios a mostrar a 10.
+$users = array_slice($users, 0, 10);
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabla de Tareas de APIs</title>
+    <title>Tabla de Usuarios de APIs</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
-    <h1 class="mt-5 text-center">Tabla de APIs</h1>
+    <h1 class="mt-5 text-center">Tabla de Usuarios</h1>
     <table class="table table-striped mt-3">
         <thead>
             <tr>
                 <th>ID</th>
-                <th class ="text-center">Título</th>
-                <th>Completado</th>
+                <th class ="text-center">Nombre</th>
+                <th>Email</th>
             </tr>
         </thead>
         <tbody>
-            <?php if (!empty($posts)): ?>
-                <?php foreach ($posts as $post): ?>
+            <?php if (!empty($users)): ?>
+                <?php foreach ($users as $user): ?>
                     <tr>
-                        <td><?php echo htmlspecialchars($post['id']); ?></td>
-                        <td><?php echo htmlspecialchars($post['title']); ?></td>
-                        <td><?php echo htmlspecialchars($post['completed'] ? 'Sí' : 'No'); ?></td>
+                        <td><?php echo htmlspecialchars($user['id']); ?></td>
+                        <td><?php echo htmlspecialchars($user['name']); ?></td>
+                        <td><?php echo htmlspecialchars($user['email']); ?></td>
                     </tr>
                 <?php endforeach; ?>
             <?php else: ?>
                 <tr>
-                    <td colspan="3">No se encontraron tareas.</td>
+                    <td colspan="3">No se encontraron usuarios.</td>
                 </tr>
             <?php endif; ?>
         </tbody>
